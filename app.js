2297c2f817e34cdf0228e3ce5c93561e43b80194
dejair-dev <62625804+dejair-dev@users.noreply.github.com>
2025-03-09 08:11:03 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades
+//  em lógica de programação. Aqui você deverá desenvolver a lógica
+//  para resolver o problema.
+
+let amigos = [];
+
+let sorteados = [];
+
+function adicionarAmigo(){
+	const input = document.getElementById('amigo')
+	let nome = input.value.trim();
+
+
+if(nome === '' || !isNaN(nome)){
+  alert("Por favor, insira um nome válido.")
+  limparCampo();
+  return;
+}
+
+  amigos.push(nome);
+  atualizarLista();
+  
+  input.value = "";
+} 
+function atualizarLista(){
+  const lista = document.getElementById('listaAmigos');
+  lista.innerHTML = '';
+ 
+     amigos.forEach((amigo,index) => {
+        const li = document.createElement('li')
+        li.textContent = amigo + (index < amigos.lenght - 1 ? ',' : "")
+        lista.appendChild(li);   
+    })
+}
+
+function sortearAmigo(){
+  if(amigos.lenght === 0){
+        alert ('A lista de amigos esta vazia.');
+        return;
+    }
+    
+    if(amigos.length <= 2){
+        alert('Para poder realizar o sorteio, é preciso ter três amigos ou mais.');
+        return;
+    }
+    
+    let indiceSorteado = Math.floor(Math.random() * amigos.length);
+    
+    let amigoSorteado = amigos[indiceSorteado];
+    
+    sorteados.push(amigoSorteado);    
+    
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "O amigo secreto sorteado é: " + sorteados;
+    
+sorteados = [];
+
+atualizarLista();
+
+}
+
+function limparCampo() {
+    lista = document.querySelector('input');
+    lista.value = '';
+}
+
+
